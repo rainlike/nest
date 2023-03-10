4e6ba1c30ca093a9dc24c878f84ac36a825c4c81
Alex Saveliev <savelevaleks@gmail.com>
2023-03-11 00:11:05 +0200

    +

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,10 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { CatsController } from './cats/cats.controller';
+import { AdminController } from './admin/admin.controller';
+import { AccountController } from './account/account.controller';
 
 @Module({
   imports: [],
-  controllers: [AppController, CatsController],
+  controllers: [AppController, CatsController, AdminController, AccountController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/cats/cats.controller.ts
+++ src/cats/cats.controller.ts
@@ -1,4 +1,61 @@
-import { Controller } from '@nestjs/common';
+import {Controller, HttpCode, Header, Redirect, Get, Post, Req, Query, Param, Body} from '@nestjs/common';
+import {Request} from 'express';
+import {Observable, of} from 'rxjs';
+import {CreateCatDto} from './dto/create-cat.dto';
 
 @Controller('cats')
-export class CatsController {}
+export class CatsController {
+    @Get()
+    findAll(@Req() request: Request): string {
+        return 'This action returns all cats';
+    }
+
+    @Get(':id/:version')
+    findOne(
+        @Param('id') id: string,
+        @Param('version') version: string
+    ): string {
+        return `This action returns a #${id} cat with version ${version}`;
+    }
+    /**
+     * @Get(':id')
+     * findOne(@Param() params): string {
+     *     return `This action returns a #${params.id} cat`;
+     * }
+     */
+
+    @Post()
+    create(@Body() createCatDto: CreateCatDto): string {
+        return 'This action adds a new cat';
+    }
+
+    @Get('wi*rd')
+    @HttpCode(204)
+    @Header('Cache-Control', 'none')
+    wildcard(): string {
+        return 'This is a wildcard action';
+    }
+
+    @Get('docs')
+    @Redirect('https://docs.nestjs.com', 302)
+    getDocs(@Query('version') version) {
+        if (version === '5') {
+            return {
+                url: 'https://docs.nestjs.com/v5',
+                statusCode: 303
+            };
+        }
+    }
+
+    /** methods `async promise` and `observable` are similar */
+    @Get('promise')
+    async promise(): Promise<any[]> {
+        return [];
+    }
+
+    /** methods `async promise` and `observable` are similar */
+    @Get('observable')
+    observable(): Observable<any[]> {
+        return of([]);
+    }
+}
