ae81cd254b515a5c959d3ea89ba0e648cb5bedb1
Alex Saveliev <savelevaleks@gmail.com>
2023-03-13 00:21:58 +0200

    ch.4 Modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,16 +1,18 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { CatsController } from './cats/cats.controller';
-import { AdminController } from './admin/admin.controller';
-import { AccountController } from './account/account.controller';
-import { WebsocketModule } from './example/websocket/websocket.module';
-import { RestModule } from './example/rest/rest.module';
-import { CatsService } from './cats/cats.service';
+import {Module} from '@nestjs/common';
+import {AppController} from './app.controller';
+import {AppService} from './app.service';
+import {AdminController} from './admin/admin.controller';
+import {AccountController} from './account/account.controller';
+import {WebsocketModule} from './example/websocket/websocket.module';
+import {RestModule} from './example/rest/rest.module';
+import {CatsModule} from './cats/cats.module';
+import {DatabaseModule} from './database/database.module';
+import {User} from './users/entities/user.entity';
 
 @Module({
-  imports: [WebsocketModule, RestModule],
-  controllers: [AppController, CatsController, AdminController, AccountController],
-  providers: [AppService, CatsService],
+  imports: [WebsocketModule, RestModule, CatsModule, DatabaseModule.forRoot([User])],
+  exports: [DatabaseModule],
+  controllers: [AppController, AdminController, AccountController],
+  providers: [AppService],
 })
 export class AppModule {}
